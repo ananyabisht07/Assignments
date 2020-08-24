c32bdbc0c0f39446dca52353e25358a45d7c11df
ananyabisht07 <ananya.bisht@yahoo.in>
2020-08-25 02:06:25 +0530

    envelop location

--- assign1/src/Component/Envelope.js
+++ assign1/src/Component/Envelope.js
@@ -1,29 +1,140 @@
 import React, { Component } from 'react'
-import { Row, Col, Image, Card } from 'react-bootstrap'
+import { Card ,Row,Col, Button} from 'react-bootstrap'
+import { CSSTransition } from 'react-transition-group';
+
+function show(clicked){
+    console.log(clicked)
+    if (clicked==1){
+        return(
+            <Card className="card-style4" >  
+                <Row>
+                    <Col className="my-auto">
+                        <h4 >Type in your location..</h4>         
+                   </Col>
+                </Row>   
+            </Card>
+        )
+    } else { return( <div></div>) }
+}
+
 
 class Envelope extends Component{
+    constructor(props) {
+        super(props)
+        this.state = { 
+            clicked: "",
+            bool: false,
+        }
+        this.handleToggleSection = this.handleToggleSection.bind(this);
+      };
+
+
+      handleToggleSection(e) {
+          const { id } = e.target;
+          this.setState(() => ({
+            clicked: id,
+            bool: true,        
+          }));
+      }
     render(){
+
+        const {clicked, bool} = this.state
         return(
-            <div className="text-center" style={{width: 'auto', marginTop:"50px" }}>
-                <img
-                    alt="envelope"
-                    width="400"
-                    style={{backgroundSize:"cover",backgroundPosition:"center"}}
-                    src="img/enve.svg" />
+            
+            <div
+                className="text-center"
+                style={{
+                marginTop: "50px",
+                position: "relative",
+                marginBottom: 500,
+                textAlign: "center",
+                }}
+            >
+                <div
+                    style={{
+                        display: "inline-block",
+                        position: "relative",
+                    }}
+                >
+
+
+
+                    <CSSTransition
+                        in={bool}
+                        timeout={1000}
+                        classNames={"slide"}
+                        unmountOnExit
+                        // onEnter={() => this.setState({ bool: false })}
+                    >
+                        <div style={{ position: "absolute", top: 190, left: 0, zIndex: -1 }}>
+                            <Card className="card-style4">
+                                <Row>
+                                <Col className="my-auto">
+                                    <h4 style={{marginTop:"45px"}}>Type in your location..</h4>
+                                </Col>
+                                </Row>
+                            </Card>
+                        </div>
+                    </CSSTransition>
+
                 
-                <h6 style={{position:"absolute", left:"32%", top:"173%"}}>Choose your location..</h6>
-                <img style={{position:"absolute", left:"47%", top:"177%"}} src="img/frame.png" width="20px" height="20px"  alt="calender"/>
-                <img style={{position:"absolute", left:"20%", top:"180%"}} src="img/offer.png" width="60px" height="60px"  alt="calender"/>
-                <img style={{position:"absolute", left:"65%", top:"180%"}} src="img/event.png" width="60px" height="60px"  alt="calender"/>
-                <h5 style={{position:"absolute", left:"21%", top:"189%"}}>EVENT</h5>
-                <h5 style={{position:"absolute", left:"65%", top:"189%"}}>OFFER</h5>
-                <Card >
+              
+                    <img alt="envelope" width="400" src="img/enve.svg" />
+                    <div role="button"
+                        onClick={() => this.setState({ bool: !bool })} 
+                        style={{ position: "absolute", top: 43, left: 115 }}>
+                        <h6>Choose your location...</h6>
+                        <img
+                            id="1"
+                            onClick={this.handleToggleSection}
+                            style={{cursor:"pointer"}}
+                            src="img/Frame.png"
+                            width="20px"
+                            height="20px"
+                            alt="calender"
+                        />
+                    </div>
+                    <div style={{ position: "absolute", top: 100, left: 50 }}>
+                        <img
+                        src="img/event.png"
+                        width="60px"
+                        height="60px"
+                        alt="calender"
+                        />
+                        <h5>EVENT</h5>
+                    </div>
+                    <div style={{ position: "absolute", right: 50, top: 100 }}>
+                        <img
+                        src="img/offer.png"
+                        width="60px"
+                        height="60px"
+                        alt="calender"
+                        />
+                        <h5>OFFER</h5>
+                    </div>     
+                </div>
+                
+                <Card className="card-style5">
                     <Card.Body>
-                        <h4>Type in your location</h4>
+                        <Row>
+                            <Col xs={4} className="text-left">
+                                <img src="img/buy.png" width="120" height="120" alt="buy" />
+                                <img 
+                                    style={{position:"absolute", top:"-10px", left:"10px"}} 
+                                    src="img/one.png" width="50" height="50" alt="one" />
+                            </Col>
+                            <Col xs={8} className="text-left">
+                                <h6>Location  :  The Biergarten</h6>
+                                <h6>Timing  :  8PM - 9PM</h6>
+                                <h6>Address :  5th block Karamangala</h6>
+                            </Col>
+                        </Row>
+                        <Row className="justify-content-center text-center">
+                            <Button className="btn-style1"  size="lg">Buy one get <br/> one free</Button><Button className="btn-style2">Redeem</Button>
+                        </Row>
                     </Card.Body>
                 </Card>
-                
-                
+                             
             </div>
         )
     }
