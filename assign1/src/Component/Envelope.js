8fd9a16fca76d37df14af2c1a8edae128a0b1ecb
ananyabisht07 <ananya.bisht@yahoo.in>
2020-08-24 23:07:56 +0530

    envelop

--- assign1/src/Component/Envelope.js
+++ assign1/src/Component/Envelope.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react'
+import { Row, Col, Image, Card } from 'react-bootstrap'
 
 class Envelope extends Component{
     render(){
         return(
-            <div className="env text-center">
-                <img src="img/env1.svg" />
+            <div className="text-center" style={{width: 'auto', marginTop:"50px" }}>
+                <img
+                    alt="envelope"
+                    width="400"
+                    style={{backgroundSize:"cover",backgroundPosition:"center"}}
+                    src="img/enve.svg" />
                 
-                    <img style={{position:"absolute",left:0}} src="img/white1.svg" />
+                <h6 style={{position:"absolute", left:"32%", top:"173%"}}>Choose your location..</h6>
+                <img style={{position:"absolute", left:"47%", top:"177%"}} src="img/frame.png" width="20px" height="20px"  alt="calender"/>
+                <img style={{position:"absolute", left:"20%", top:"180%"}} src="img/offer.png" width="60px" height="60px"  alt="calender"/>
+                <img style={{position:"absolute", left:"65%", top:"180%"}} src="img/event.png" width="60px" height="60px"  alt="calender"/>
+                <h5 style={{position:"absolute", left:"21%", top:"189%"}}>EVENT</h5>
+                <h5 style={{position:"absolute", left:"65%", top:"189%"}}>OFFER</h5>
+                <Card >
+                    <Card.Body>
+                        <h4>Type in your location</h4>
+                    </Card.Body>
+                </Card>
+                
                 
             </div>
         )
